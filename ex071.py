7de24042e7e977153969722f76b50954549088a6
heltonsdl <101609798+heltonsdl@users.noreply.github.com>
2022-03-16 22:42:20 -0300

    Update ex071.py
    Incluir a descrição do exercício 71

--- ex071.py
+++ ex071.py
@@ -1,5 +1,9 @@
 from time import sleep
 
+'''Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+
+considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
+
 cont50 = cont20 = cont10 = cont1 = 0
 print(' ------ Simulador de Caixa ------ ')
 valor = int(input(' Digite o valor: R$'))
